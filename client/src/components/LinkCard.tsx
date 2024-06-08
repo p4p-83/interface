246498a3950f3c4df78a42bdc9f18cfd5194b4d3
James <james@jamesnzl.xyz>
2024-06-08 22:54:10 +1200

    feat: :sparkles: add home page

---
+++ client/src/components/LinkCard.tsx
@@ -0,0 +1,37 @@
+import { SymbolIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+interface LinkCardProps extends React.HTMLAttributes<HTMLAnchorElement> {
+  href: string;
+  title: string;
+  description: string;
+  icon: typeof SymbolIcon;
+}
+
+export const LinkCard = ({ className, href, title, description, icon: Icon }: LinkCardProps) => {
+  return (
+    <Link href={href} className={className}>
+      <Card className='group hover:border-primary transition-colors duration-150'>
+
+        <CardHeader className='pb-3'>
+          <CardTitle className='flex flex-row gap-2 justify-start items-center text-primary transition-colors duration-150 text-2xl font-bold uppercase'>
+            <Icon className='p-0 h-5 w-5 shrink-0' strokeWidth={0.5} stroke="currentColor" />
+            {title}
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <p className='group-hover:text-primary transition-colors duration-150 text-lg leading-8'>{description}</p>
+        </CardContent>
+
+      </Card>
+    </Link>
+  )
+}
