59e63abdaf5ccbeeaf050625a93d57fe05517fe9
James <james@jamesnzl.xyz>
2024-06-08 19:13:56 +1200

    feat: :lipstick: make circle smaller

--- client/script.js
+++ client/script.js
@@ -1,3 +1,7 @@
+const CONSTANTS = {
+	CIRCLE_SIZE: 10,
+};
+
 document.addEventListener('DOMContentLoaded', () => {
 	const clickCircle = document.getElementById('click-circle');
 	const followCircle = document.getElementById('follow-circle');
@@ -6,16 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Show the circle at the click position
 	document.body.addEventListener('click', (e) => {
-		clickCircle.style.left = `${e.clientX - 25}px`;
-		clickCircle.style.top = `${e.clientY - 25}px`;
+		clickCircle.style.left = `${e.clientX - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
+		clickCircle.style.top = `${e.clientY - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
 		clickCircle.style.display = 'block';
 		clickCirclePosition = { x: e.clientX, y: e.clientY };
 	});
 
 	// Make the circle follow the mouse
 	document.body.addEventListener('mousemove', (e) => {
-		followCircle.style.left = `${e.clientX - 25}px`;
-		followCircle.style.top = `${e.clientY - 25}px`;
+		followCircle.style.left = `${e.clientX - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
+		followCircle.style.top = `${e.clientY - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
 	});
 
 	// Log the position of the click circle on submit
