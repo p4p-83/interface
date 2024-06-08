4f2a6ade7cb523e584cbac313204619138f263dd
James <james@jamesnzl.xyz>
2024-06-08 20:26:23 +1200

    feat: :sparkles: remove submit button

--- client/script.js
+++ client/script.js
@@ -31,7 +31,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 	const clickCircle = document.getElementById('click-circle');
 	const followCircle = document.getElementById('follow-circle');
-	const submitButton = document.getElementById('submit-button');
 	let clickCirclePosition = { x: 0, y: 0 };
 
 	// Show the circle at the click position
@@ -40,18 +39,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 		clickCircle.style.top = `${e.clientY - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
 		clickCircle.style.display = 'block';
 		clickCirclePosition = { x: e.clientX, y: e.clientY };
+
+		console.log(`Circle position: X: ${clickCirclePosition.x}, Y: ${clickCirclePosition.y}`);
 	});
 
 	// Make the circle follow the mouse
 	document.body.addEventListener('mousemove', (e) => {
 		followCircle.style.left = `${e.clientX - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
 		followCircle.style.top = `${e.clientY - (CONSTANTS.CIRCLE_SIZE / 2)}px`;
-	});
-
-	// Log the position of the click circle on submit
-	submitButton.addEventListener('click', (e) => {
-		console.log(`Circle position: X: ${clickCirclePosition.x}, Y: ${clickCirclePosition.y}`);
-		e.preventDefault();
-		e.stopImmediatePropagation();
 	});
 });
