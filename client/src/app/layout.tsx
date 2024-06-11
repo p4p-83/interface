61b952cf662e8fa075d0964267d99fe8e1cf4288
James <james@jamesnzl.xyz>
2024-06-12 03:45:44 +1200

    feat: :lipstick: change font to encode sans

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Encode_Sans } from 'next/font/google'
 import './globals.css'
 
 import { Toaster } from '@/components/ui/sonner'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { cn } from '@/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+const encodeSans = Encode_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'PnP << 83',
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body className={cn(inter.className, 'flex min-h-screen flex-col items-center justify-center px-5')}>
+      <body className={cn(encodeSans.className, 'flex min-h-screen flex-col items-center justify-center px-5')}>
 
         <ThemeProvider
           attribute='class'
