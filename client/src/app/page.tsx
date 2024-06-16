178f47580063f795a5f65c9f303a416d76f08e22
James <james@jamesnzl.xyz>
2024-06-16 13:14:44 +1200

    feat: :speech_balloon: update subtitle format specifier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,7 +9,7 @@ const SUBTITLES = [
   '~ echo "ECSE Part IV Project #83"',
   'char project[] = "ECSE Part IV Project #83";',
   '~ [ "$number" -eq 83 ] && echo "ECSE Part IV Project #$number"',
-  'printf("ECSE Part IV Project #%d\\n", 83);',
+  'printf("ECSE Part IV Project #%u\\n", 83);',
   'std::cout << "ECSE Part IV Project #83" << std::endl;',
 ] as const
 
