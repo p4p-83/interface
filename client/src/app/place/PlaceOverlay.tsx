edbb38c9e67b3e79d301a67be6a347992cd3e64a
James <james@jamesnzl.xyz>
2024-07-01 17:40:45 +1200

    feat: :sparkles: implement 'exponential runaway' nearest target algorithm

--- client/src/app/place/PlaceOverlay.tsx
+++ client/src/app/place/PlaceOverlay.tsx
@@ -19,7 +19,104 @@ export function PlaceOverlay({ socketUrl, overlaySize, circleSize, hideOverlay =
   const didUnmount = useRef(false)
 
   const [targetOffset, setTargetOffset] = useState<Position | null>(null)
-  const [targetPositionOffsets, setTargetPositionOffsets] = useState<Position[] | null>(null)
+  const [targetPositionOffsets, setTargetPositionOffsets] = useState<Position[] | null>([
+    {
+      'x': 0.09790188448920424,
+      'y': 0.9416189822232395,
+    },
+    {
+      'x': 0.24185549706263829,
+      'y': 0.9050278477149615,
+    },
+    {
+      'x': 0.8876020447089341,
+      'y': 0.8755168993667506,
+    },
+    {
+      'x': 0.7434653238727398,
+      'y': 0.4303349355306325,
+    },
+    {
+      'x': 0.1125810635538262,
+      'y': 0.5462272068360418,
+    },
+    {
+      'x': 0.15208667124437322,
+      'y': 0.37766079194323643,
+    },
+    {
+      'x': 0.5499656672007325,
+      'y': 0.5133745326924544,
+    },
+    {
+      'x': 0.925963225757229,
+      'y': 0.7672541390096895,
+    },
+    {
+      'x': 0.5512626840619517,
+      'y': 0.558434424353399,
+    },
+    {
+      'x': 0.4457770656900893,
+      'y': 0.7488212405584802,
+    },
+    {
+      'x': 0.9060502021820401,
+      'y': 0.03494316014343481,
+    },
+    {
+      'x': 0.7314259555962462,
+      'y': 0.05574120698863203,
+    },
+    {
+      'x': 0.5562523842221714,
+      'y': 0.3819943541618982,
+    },
+    {
+      'x': 0.30038910505836575,
+      'y': 0.5775082017242694,
+    },
+    {
+      'x': 0.4777904936293584,
+      'y': 0.43222705424582286,
+    },
+    {
+      'x': 0.30475318532082096,
+      'y': 0.2430304417486839,
+    },
+    {
+      'x': 0.1262073701075761,
+      'y': 0.24333562218661783,
+    },
+    {
+      'x': 0.12397955291065843,
+      'y': 0.3066453040360113,
+    },
+    {
+      'x': 0.7494621194781415,
+      'y': 0.028595407034409093,
+    },
+    {
+      'x': 0.7555809872587167,
+      'y': 0.5491416800183109,
+    },
+    {
+      'x': 0.8394750896467537,
+      'y': 0.684290836957351,
+    },
+    {
+      'x': 0.03205920500495918,
+      'y': 0.861326009002823,
+    },
+    {
+      'x': 0.8349889372091249,
+      'y': 0.7737087052719921,
+    },
+    {
+      'x': 0.5819943541618983,
+      'y': 0.4532539864194705,
+    },
+  ])
 
   // Unmount
   useEffect(() => {
@@ -73,6 +170,7 @@ export function PlaceOverlay({ socketUrl, overlaySize, circleSize, hideOverlay =
           break
 
         case 'DRAW_TARGETS':
+          console.log(action.payload)
           setTargetPositionOffsets(action.payload)
           break
 
@@ -198,8 +296,9 @@ export function PlaceOverlay({ socketUrl, overlaySize, circleSize, hideOverlay =
            * Shift+Direction will cause the gantry to step a fixed distance in the specified direction
            */
 
+          const previousOffset = targetOffset ?? { x:0.5, y:0.5 }
+
           if (event.shiftKey) {
-            const previousOffset = targetOffset ?? { x:0.5, y:0.5 }
 
             const unclampedOffset = { ...previousOffset }
             switch (event.code) {
@@ -250,9 +349,59 @@ export function PlaceOverlay({ socketUrl, overlaySize, circleSize, hideOverlay =
               setTargetOffset(clampedOffset)
               socket.sendTargetDeltas(webSocket, clampedOffset)
             }
+
           }
           else {
-            // TODO:
+
+            if (!targetPositionOffsets?.length) return
+
+            let filterPredicate: (value: Position) => boolean
+            let sortingFunction: (targetPosition: Position) => number
+            switch (event.code) {
+            case 'KeyS':
+            case 'ArrowDown':
+            case 'KeyJ':
+              filterPredicate = (value) => value.y > previousOffset.y
+              sortingFunction = (targetPosition) => ((targetPosition.y * 100) - (previousOffset.y * 100)) + (((targetPosition.x * 100) - (previousOffset.x * 100)) ** 2)
+              break
+            case 'KeyW':
+            case 'ArrowUp':
+            case 'KeyK':
+              filterPredicate = (value) => value.y < previousOffset.y
+              sortingFunction = (targetPosition) => ((previousOffset.y * 100) - (targetPosition.y * 100)) + (((previousOffset.x * 100) - (targetPosition.x * 100)) ** 2)
+              break
+            case 'KeyA':
+            case 'ArrowLeft':
+            case 'KeyH':
+              filterPredicate = (value) => value.x < previousOffset.x
+              sortingFunction = (targetPosition) => ((previousOffset.x * 100) - (targetPosition.x * 100)) + (((previousOffset.y * 100) - (targetPosition.y * 100)) ** 2)
+              break
+            case 'KeyD':
+            case 'ArrowRight':
+            case 'KeyL':
+              filterPredicate = (value) => value.x > previousOffset.x
+              sortingFunction = (targetPosition) => ((targetPosition.x * 100) - (previousOffset.x * 100)) + (((targetPosition.y * 100) - (previousOffset.y * 100)) ** 2)
+              break
+            case 'KeyR':
+              setTargetOffset(null)
+              return
+            case 'Space':
+              targetOffset && socket.sendTargetDeltas(webSocket, targetOffset)
+              return
+            default:
+              return
+            }
+
+            // Find nearest target
+            const nearestTargetOffset = targetPositionOffsets
+              .filter(filterPredicate)
+              .toSorted((a, b) => sortingFunction(a) - sortingFunction(b))
+              [0]
+
+            // Go there
+            console.info('Found: ', nearestTargetOffset)
+            setTargetOffset(nearestTargetOffset)
+
           }
         }}
       >
@@ -260,7 +409,7 @@ export function PlaceOverlay({ socketUrl, overlaySize, circleSize, hideOverlay =
         {/* Target circle */}
         {(targetOffset) && (
           <div
-            className='absolute opacity-75 bg-ring outline outline-1 outline-primary-foreground rounded-full pointer-events-none cursor-crosshair'
+            className='absolute z-50 opacity-75 bg-ring outline outline-1 outline-primary-foreground rounded-full pointer-events-none cursor-crosshair'
             style={{
               width: circleSize,
               height: circleSize,
@@ -274,7 +423,7 @@ export function PlaceOverlay({ socketUrl, overlaySize, circleSize, hideOverlay =
         {(targetPositionOffsets?.length) && targetPositionOffsets.map((position, index) => (
           <div
             key={index}
-            className='absolute opacity-50 bg-ring outline outline-1 outline-primary-foreground rounded-none cursor-pointer'
+            className='absolute z-0 opacity-25 bg-accent-foreground outline outline-1 outline-accent rounded-none cursor-pointer'
             onMouseDown={(event: MouseEvent) => {
               console.info(`Clicked target at position (${position.x}, ${position.y})`)
               event.stopPropagation()
