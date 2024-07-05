301646842a8c254548b8e964ba2edccbf9b68959
James <james@jamesnzl.xyz>
2024-07-06 01:48:43 +1200

    fix: :lipstick: add `LayoutMain` around 404 page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,12 +1,15 @@
 import { Error } from '@/components/Error'
+import { LayoutMain } from '@/components/LayoutMain'
 import { TypographyLarge, TypographyMuted } from '@/components/ui/typography'
 
 export default function NotFound() {
   return (
-    <Error>
-      <TypographyLarge>404.</TypographyLarge>
-      <br />
-      <TypographyMuted>This page could not be found.</TypographyMuted>
-    </Error>
+    <LayoutMain>
+      <Error>
+        <TypographyLarge>404.</TypographyLarge>
+        <br />
+        <TypographyMuted>This page could not be found.</TypographyMuted>
+      </Error>
+    </LayoutMain>
   )
 }
