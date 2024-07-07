f32a29277d954664d818747f7d2b19f6c48b95ee
James <james@jamesnzl.xyz>
2024-07-08 08:10:51 +1200

    feat: :sparkles: add header to 404 page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,15 +1,20 @@
 import { Error } from '@/components/Error'
+import { Header } from '@/components/Header'
 import { LayoutMain } from '@/components/LayoutMain'
 import { TypographyLarge, TypographyMuted } from '@/components/ui/typography'
 
 export default function NotFound() {
   return (
     <LayoutMain>
+
+      <Header />
+
       <Error>
         <TypographyLarge>404.</TypographyLarge>
         <br />
         <TypographyMuted>This page could not be found.</TypographyMuted>
       </Error>
+
     </LayoutMain>
   )
 }
