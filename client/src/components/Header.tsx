b2e83e8033a2140ee32aa1e1b5f13d0653090a01
James <james@jamesnzl.xyz>
2024-07-07 13:42:31 +1200

    feat: :sparkles: add mobile navigation menu

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,56 +2,121 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Crosshair2Icon } from '@radix-ui/react-icons'
+import { useState } from 'react'
+import { Cross2Icon, Crosshair2Icon, HamburgerMenuIcon } from '@radix-ui/react-icons'
 
 import * as GLOBALS from '@/app/globals'
 import { ThemeToggle } from '@/components/ThemeToggle'
+import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from '@/components/ui/sheet'
 import { cn } from '@/lib/utils'
 
-export const Header = () => {
+function HeaderTitle() {
+  return (
+    <h1 className='flex flex-row gap-2 justify-start items-center text-xl md:text-2xl md:text-[1.75rem] font-extrabold underline decoration-primary'>
+      <Crosshair2Icon className='p-0 h-4 md:h-5 w-4 md:w-5 shrink-0' strokeWidth={0.5} stroke='currentColor' />
+      <span>p4p-83/interface</span>
+    </h1>
+  )
+}
+
+export function Header() {
   return (
     <header className='fixed bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-card/60 text-card-foreground border-b shadow top-0 left-0 right-0 z-40'>
-      <div className='flex items-center justify-between px-6 my-0 mx-auto h-28 max-w-5xl'>
+      <div className='flex items-center justify-between px-6 my-0 mx-auto h-20 sm:h-28 max-w-5xl'>
 
         <Link href={GLOBALS.PAGES.HOME.path} className='p-2 md:p-4 select-none cursor-pointer m-0 inline-flex items-end'>
-          <h1 className='flex flex-row gap-2 justify-start items-center text-xl md:text-2xl md:text-[1.75rem] font-extrabold underline decoration-primary'>
-            <Crosshair2Icon className='p-0 h-4 md:h-5 w-4 md:w-5 shrink-0' strokeWidth={0.5} stroke='currentColor' />
-            <span className='block sm:hidden'>{GLOBALS.PAGES.TITLE_SUFFIX}</span>
-            <span className='hidden sm:block'>A rapid pick-and-place</span>
-          </h1>
+          <HeaderTitle />
         </Link>
 
-        <nav className='inline-flex flex-row items-center justify-between align-middle gap-x-0.5 lg:gap-x-5'>
-
-          <NavItem hideWhenSmall href={GLOBALS.PAGES.HOME.path} label={GLOBALS.PAGES.HOME.name} />
-          <NavItem href={GLOBALS.PAGES.PLACE.path} label={GLOBALS.PAGES.PLACE.name} />
-          <NavItem href={GLOBALS.PAGES.LEARN.path} label={GLOBALS.PAGES.LEARN.name} />
-          <NavItem href={GLOBALS.PAGES.PROJECT.path} label={GLOBALS.PAGES.PROJECT.name} />
-
-          <div className='pl-2 lg:pl-3'>
-            <ThemeToggle />
-          </div>
-
-        </nav>
+        <NavigationMain />
+        <NavigationMobile />
 
       </div>
     </header>
   )
 }
 
-export type NavItemProps = {
-  hideWhenSmall?: boolean;
+function NavigationMain() {
+  return (
+    <nav className='hidden sm:inline-flex flex-row items-center justify-between align-middle gap-x-0.5 lg:gap-x-5'>
+
+      <NavigationItem href={GLOBALS.PAGES.HOME.path} label={GLOBALS.PAGES.HOME.name} />
+      <NavigationItem href={GLOBALS.PAGES.PLACE.path} label={GLOBALS.PAGES.PLACE.name} />
+      <NavigationItem href={GLOBALS.PAGES.LEARN.path} label={GLOBALS.PAGES.LEARN.name} />
+      <NavigationItem href={GLOBALS.PAGES.PROJECT.path} label={GLOBALS.PAGES.PROJECT.name} />
+
+      <div className='pl-2 lg:pl-3'>
+        <ThemeToggle />
+      </div>
+
+    </nav>
+  )
+}
+
+export function NavigationMobile() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+
+      <Sheet open={open} onOpenChange={setOpen}>
+        <div className='sm:hidden inline-flex flex-row gap-4'>
+          <ThemeToggle />
+
+          <SheetTrigger asChild>
+            <Button variant='outline' size='icon'>
+              <HamburgerMenuIcon className='h-[1.2rem] w-[1.2rem]' />
+              <span className='sr-only'>Toggle Menu</span>
+            </Button>
+          </SheetTrigger>
+        </div>
+
+        <SheetContent side='left' className='pt-0' noCloseButton>
+
+          <div className='flex items-center justify-between gap-4 h-20'>
+
+            <Link href={GLOBALS.PAGES.HOME.path} className='p-2 md:p-4 grow select-none cursor-pointer m-0 inline-flex items-center'>
+              <HeaderTitle />
+            </Link>
+
+            <SheetClose asChild>
+              <Button variant='outline' size='icon' className='sm:hidden'>
+                <Cross2Icon className='h-[1.2rem] w-[1.2rem]' />
+                <span className='sr-only'>Close</span>
+              </Button>
+            </SheetClose>
+
+          </div>
+
+          <div className='flex flex-col my-4 pl-6 space-y-3'>
+            {/* TODO: other links? */}
+            {Object.values(GLOBALS.PAGES)
+              .filter(page => (page instanceof Object) && ('path' in page))
+              .map(page => (
+                <NavigationItem key={page.path} href={page.path} label={page.name} />
+              ))}
+          </div>
+
+        </SheetContent>
+      </Sheet>
+
+    </>
+  )
+}
+
+type NavigationItemProps = {
   href: string;
   label: string;
 };
 
-export const NavItem = ({ href, label, hideWhenSmall }: NavItemProps) => {
+function NavigationItem({ href, label }: NavigationItemProps) {
   const pathname = usePathname()
 
   let linkStyle = cn(
-    'py-4 px-2 lg:px-4',
+    'py-2 first:pt-0 sm:py-4 sm:first:pt-4 px-0 sm:px-2 lg:px-4',
     'transition-colors',
-    'font-medium uppercase text-sm md:text-base',
+    'font-medium uppercase text-sm sm:text-base',
     'select-none cursor-pointer'
   )
   // Non-active link
@@ -65,10 +130,6 @@ export const NavItem = ({ href, label, hideWhenSmall }: NavItemProps) => {
 
   // Hover effects
   const hoverStyle = cn('hover:cursor-pointer', 'hover:text-primary/90')
-
-  if (hideWhenSmall) {
-    linkStyle = cn(linkStyle, 'hidden md:block')
-  }
 
   return (
     <Link href={href} className={cn(linkStyle, hoverStyle)}>
--- client/src/components/LayoutMain.tsx
+++ client/src/components/LayoutMain.tsx
@@ -11,8 +11,8 @@ export function LayoutMain({ children, justifyStart = false }: LayoutMainProps) 
   return (
 
     <main className={cn(
-      'flex min-h-screen flex-col items-center px-5',
-      (justifyStart) ? 'justify-start has-[header]:py-40' : 'justify-center has-[header]:py-32',
+      'flex min-h-screen flex-col items-center px-6',
+      (justifyStart) ? 'justify-start has-[header]:py-28 sm:has-[header]:py-40' : 'justify-center has-[header]:py-32',
     )}>
       {children}
     </main>
