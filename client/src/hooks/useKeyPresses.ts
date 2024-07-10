3648658448707729c1ee424fc0644ee702c67a93
James <james@jamesnzl.xyz>
2024-07-11 02:18:21 +1200

    fix: :bug: disable `useKeyPresses()` inside input elements by default

--- client/src/hooks/useKeyPresses.ts
+++ client/src/hooks/useKeyPresses.ts
@@ -3,16 +3,22 @@ import { useEffect, useState } from 'react'
 type UseKeyPressesProps = {
 	onKeyDown?: (event: KeyboardEvent) => void;
 	onKeyUp?: (event: KeyboardEvent) => void;
+  includeInputElements?: boolean;
 }
 
 export function useKeyPresses(props?: UseKeyPressesProps) {
-  const { onKeyDown, onKeyUp } = props ?? {}
+  const { onKeyDown, onKeyUp, includeInputElements } = props ?? {}
 
   const [isShiftPressed, setIsShiftPressed] = useState(false)
   const [isOptionPressed, setIsOptionPressed] = useState(false)
 
   useEffect(() => {
     function handleKeyDown(event: KeyboardEvent) {
+      if (!includeInputElements && isInputEvent(event)) {
+        console.info(`Ignored input key down for ${event.code} (${event.key})`, event)
+        return
+      }
+
       console.info(`Key down for ${event.code} (${event.key})`, event)
       setIsShiftPressed(event.shiftKey)
       setIsOptionPressed(event.altKey)
@@ -20,6 +26,11 @@ export function useKeyPresses(props?: UseKeyPressesProps) {
     }
 
     function handleKeyUp(event: KeyboardEvent) {
+      if (!includeInputElements && isInputEvent(event)) {
+        console.info(`Ignored input key up for ${event.code} (${event.key})`, event)
+        return
+      }
+
       console.info(`Key up for ${event.code} (${event.key})`, event)
       setIsShiftPressed(event.shiftKey)
       setIsOptionPressed(event.altKey)
@@ -33,10 +44,17 @@ export function useKeyPresses(props?: UseKeyPressesProps) {
       document.removeEventListener('keydown', handleKeyDown)
       document.removeEventListener('keyup', handleKeyUp)
     }
-  }, [onKeyDown, onKeyUp])
+  }, [onKeyDown, onKeyUp, includeInputElements])
 
   return {
     isShiftPressed,
     isOptionPressed,
   } as const
+}
+
+const INPUT_TAGS = ['input', 'textarea']
+function isInputEvent(event: KeyboardEvent) {
+  return (
+    (event.target instanceof Element) && INPUT_TAGS.includes(event.target.tagName.toLowerCase())
+  )
 }
